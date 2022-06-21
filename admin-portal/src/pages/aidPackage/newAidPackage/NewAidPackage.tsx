5737838f912bb98e8fcae50eca513acd4b3c3232
Ryan Kuruppu <ryanjk.kuruppu@gmail.com>
2022-06-21 13:36:39 +0530

    Updated NewAidPackage Page component to fetch requirement data from API and render it in relevant table component

--- admin-portal/src/pages/aidPackage/newAidPackage/NewAidPackage.tsx
+++ admin-portal/src/pages/aidPackage/newAidPackage/NewAidPackage.tsx
@@ -4,45 +4,45 @@ import { PageSelection } from "types/pages";
 import { Page } from "layout/page";
 
 import "./newAidPackage.css";
+import MedicalRequirementsTable from "components/medicalRequirementsTable/MedicalRequirementsTable";
+import MedicalNeedService from "services/medicalNeedService";
+import { useQuery } from "react-query";
+import CreateAidPackageTable from "components/createAidPackageTable/CreateAidPackageTable";
 
-type Props = {};
-
-function NewAidPackage({}: Props) {
+function NewAidPackage() {
+  const medicalNeedService = new MedicalNeedService();
   const [currentFormStep, setCurrentFormStep] = useState(0);
+
+  const {
+    data: medicalRequirements,
+    isLoading,
+    isError,
+  } = useQuery("medicalNeeds", () => medicalNeedService.getMedicalNeeds());
+
+  function handleStepChange() {
+    setCurrentFormStep((prevStep) => (prevStep === 0 ? 1 : 0));
+  }
 
   return (
     <Page selection={PageSelection.PACKAGE_CREATION}>
-      <main className="">
+      <main>
         <h1>Create an Aid Package</h1>
         <Stepper activeStep={currentFormStep}>
           <Step title="Assign Suppliers" />
           <Step title="Manage Aid Packages" />
         </Stepper>
+        {isLoading && <p>Loading...</p>}
+        {isError && <p>There was an issue when we tried to get your data...</p>}
+        {!isLoading && currentFormStep === 0 && (
+          <MedicalRequirementsTable requirements={medicalRequirements} />
+        )}
+        {!isLoading && currentFormStep === 1 && <CreateAidPackageTable />}
+        <button onClick={handleStepChange}>
+          {currentFormStep === 0 ? "Next" : "Previous"}
+        </button>
       </main>
     </Page>
   );
 }
-
-// {
-//   currentFormStep == 0 && (
-//     <table>
-//       <thead>
-//         <tr>
-//           <td>Need</td>
-//           <td>Unit</td>
-//           <td>Period</td>
-//           <td>Quantity Needed</td>
-//           <td>Remaining Needed</td>
-//           <td>Supplier</td>
-//           <td>Max</td>
-//           <td>Order Quantity</td>
-//         </tr>
-//       </thead>
-//       <tbody>
-//         <tr>{/** Enumerate over required items */}</tr>
-//       </tbody>
-//     </table>
-//   );
-// }
 
 export default NewAidPackage;
